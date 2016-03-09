27de02741348b4a42262e7ccc2d54b67cfcd6919
ivpi <ivpi@itu.dk>
2016-03-09 18:53:06 +0100

    volt: blocks initialization

--- hw/block/volt_ssd.c
+++ hw/block/volt_ssd.c
@@ -6,14 +6,32 @@
 void * nvme_volt_main(void *ctrl) {
     NvmeCtrl *n = (NvmeCtrl *) ctrl;
     LnvmVoltCtrl *volt = &n->volt_ctrl;
-    printf("volt: Volatile SSD started succesfully!\n");
-    while(volt->status.active){
+    
+    while(volt->status.active){     
         printf("\nvolt: I am alive!\n");
-        printf("%d channel(s)\n%d lun(s) per channel\n%d block(s) per lun\n%d page(s) per block.\n",
-                volt->params.num_ch, volt->params.num_lun, volt->params.num_blk, volt->params.num_pg);
-        sleep(2);
+        printf("%d channel(s)\n%d lun(s) per channel\n%d block(s) per lun\n%d page(s) per block\nPage size: %d\n",
+            volt->params.num_ch, volt->params.num_lun, volt->params.num_blk, volt->params.num_pg, (int) volt->params.pg_size);
+        sleep(2000);
     }
     return n;
+}
+
+static int nvme_volt_init_page(int c){
+    return ++c;
+}
+
+static int nvme_volt_init_blocks(LnvmVoltCtrl *volt){    
+    /* for now, only LUNs*/
+    int c = 0;
+    int i_blk;
+    int i_pg;
+    int total_blk = volt->params.num_blk*volt->params.num_lun;
+    for(i_blk = 0; i_blk <= total_blk; i_blk++){
+        for(i_pg = 0; i_pg <= volt->params.num_blk; i_pg++){
+            c = nvme_volt_init_page(c);
+        }        
+    }
+    return c;
 }
 
 void nvme_volt_init(void *ctrl)
@@ -31,11 +49,16 @@ void nvme_volt_init(void *ctrl)
     volt->params.num_lun = c->num_lun;
     volt->params.num_blk = c->num_blk;
     volt->params.num_pg = c->num_pg;
-    volt->status.active = 1; // activated
-    volt->status.ready = 0; // busy
+    volt->params.pg_size = c->fpg_sz;
+    volt->status.active = 1; /* activated */
+    volt->status.ready = 0; /* busy */
+    
+    /* Memory allocation */
+    int init_pages = nvme_volt_init_blocks(volt);
+    printf("\nvolt: Volatile SSD started succesfully with %d pages.\n", init_pages);
     
     pthread_t pth;
     int res = pthread_create(&pth, NULL, nvme_volt_main, n);
     if (res)
-        volt->status.active = 0; //disabled
+        volt->status.active = 0; /* disabled */
 }
--- hw/block/volt_ssd.h
+++ hw/block/volt_ssd.h
@@ -8,20 +8,43 @@ typedef struct LnvmVoltParams {
     uint8_t     num_lun;
     uint16_t    num_blk;
     uint16_t    num_pg;
+    size_t      pg_size;
 } LnvmVoltParams;
 
 typedef struct LnvmVoltStatus {
-    uint8_t     ready; // 0-busy, 1-ready to use
-    uint8_t     active; // 0-disabled, 1-activated
+    uint8_t     ready; /* 0-busy, 1-ready to use */
+    uint8_t     active; /* 0-disabled, 1-activated */
 } LnvmVoltStatus;
+
+typedef struct LnvmVoltPage {
+    void *data;
+    uint8_t state; /* 0-free, 1-alive, 2-invalid */
+    void *block;
+} LnvmVoltPage;
+
+typedef struct LnvmVoltBlock {
+    LnvmVoltPage *pages;
+    uint16_t life; /* available writes before die */
+    void *lun;
+} LnvmVoltBlock;
+
+typedef struct LnvmVoltLun {
+    LnvmVoltBlock *blocks;
+    void *channel;
+} LnvmVoltLun;
+
+typedef struct LnvmVoltChannel {
+    LnvmVoltLun *luns;
+} LnvmVoltCh;
 
 typedef struct LnvmVoltCtrl {
     LnvmVoltParams params;
     LnvmVoltStatus status;
+    LnvmVoltBlock * blocks;
 } LnvmVoltCtrl;
 
 /* TODO: after the work is done, change void to NvmeCtrl */
 void nvme_volt_init(void *ctrl);
-void * nvme_volt_main(void *ctrl);
+void * nvme_volt_main(void *ctrl); /* main thread */
 
 #endif /* VOLT_SSD_H */ 
